e440ec8f01c1b1bcccac0b4aa37b5494c7003f07
Shindou <98887570+Shindou7@users.noreply.github.com>
2024-03-27 22:13:35 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,51 +1,46 @@
 #!/usr/bin/python3
 
-"""Script that reads stdin line by line and computes metrics"""
+""" script that reads stdin"""
 
 import sys
-import signal
 
-def print_stats(status_counts, total_size):
-    """Prints the file size and the number of lines for each status code"""
-    print("File size: {:d}".format(total_size))
-    for status_code in sorted(status_counts.keys()):
-        count = status_counts[status_code]
-        if count != 0:
-            print("{}: {:d}".format(status_code, count))
 
-def signal_handler(sig, frame):
-    """Handles the keyboard interrupt signal"""
-    print_stats(status_counts, total_size)
-    sys.exit(0)
+def printStatus(dic, size):
+    """ Prints information """
+    print("File size: {:d}".format(size))
+    for i in sorted(dic.keys()):
+        if dic[i] != 0:
+            print("{}: {:d}".format(i, dic[i]))
 
-status_counts = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0, "404": 0, "405": 0, "500": 0}
-total_size = 0
-line_number = 0
 
-signal.signal(signal.SIGINT, signal_handler)
+# sourcery skip: use-contextlib-suppress
+statusCodes = {"200": 0, "301": 0, "400": 0, "401": 0, "403": 0,
+               "404": 0, "405": 0, "500": 0}
+
+count = 0
+size = 0
 
 try:
     for line in sys.stdin:
-        line_number += 1
-        if line_number % 10 == 0:
-            print_stats(status_counts, total_size)
+        if count != 0 and count % 10 == 0:
+            printStatus(statusCodes, size)
 
-        parts = line.split()
+        stlist = line.split()
+        count += 1
+
         try:
-            status_code = parts[-2]
-            if status_code in status_counts:
-                status_counts[status_code] += 1
-        except IndexError:
+            size += int(stlist[-1])
+        except Exception:
             pass
 
         try:
-            file_size = int(parts[-1])
-            total_size += file_size
-        except (ValueError, IndexError):
+            if stlist[-2] in statusCodes:
+                statusCodes[stlist[-2]] += 1
+        except Exception:
             pass
+    printStatus(statusCodes, size)
 
-    print_stats(status_counts, total_size)
 
 except KeyboardInterrupt:
-    print_stats(status_counts, total_size)
-    sys.exit(0)
+    printStatus(statusCodes, size)
+    raise
